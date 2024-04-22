159c53d727a94de71e2a3ccb22fe24048673297b
DooxFonggg <phongcoi1235@gmail.com>
2024-04-22 23:30:07 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('App bar'),
-            backgroundColor: Colors.red,
-          ),
-          body: SafeArea(
-            child: Center(
-              child: Text("center text"),
-            ),
-          )),
-    ),
-  );
+    appBar: AppBar(title: Text('Fong app')),
+    body: Body(),
+  )));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        child: Text('hello im fong'),
+        // color: Colors.amberAccent,
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue, width: 10),
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(8, 8),
+                color: Colors.pink,
+                blurRadius: 10,
+              )
+            ]),
+      ),
+    );
+  }
 }
