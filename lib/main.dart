8eee0623a465a6210c9d09655e56d3a307de9964
DooxFonggg <phongcoi1235@gmail.com>
2024-04-29 17:23:57 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -2,65 +2,69 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: SafeArea(
-          child: Scaffold(
-    body: Body(),
-  ))));
+    home: SafeArea(
+        child: Scaffold(
+      appBar: AppBar(title: Text('Hello World')),
+      body: Body2(),
+    )),
+  ));
 }
 
 class Body extends StatelessWidget {
-  const Body({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.only(top: 20),
-              child: Column(
-                children: [Text('Home'), Text("About"), Text("Contact")],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-                padding: EdgeInsets.only(top: 20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text("yyyy/mm/dd"),
-                        Expanded(
-                            flex: 2,
-                            child: Container(
-                                child: Row(
-                              children: [Text("yyyy")],
-                            ))),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            child: Row(
-                              children: [Text("mm")],
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            child: Text("dd"),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                )),
-          )
-        ],
+    return GridView.count(
+      crossAxisCount: 1,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      padding: EdgeInsets.all(10),
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.red),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.red),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.red),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.red),
+        )
+      ],
+    );
+  }
+}
+
+class Body2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 100,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        childAspectRatio: 1 / 0.5,
       ),
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.red),
+        );
+      },
+      itemCount: 10,
     );
   }
 }
